b73e0c6a561769f458f82b5af52fcc16e45d0e0a
Eric Hedgren <ehedgren@iastate.edu>
2023-10-10 12:23:55 -0500

    add test to main. need to figure out how to add seed

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String args[]){
         BloomFilterFNV bf = new BloomFilterFNV(100, 10);
-        bf.add("A");
+        bf.add("Afwfe");
         System.out.println(bf.bitArray);
         bf.add("B");
         System.out.println((bf.bitArray));
