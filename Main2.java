6be487193e6b67f7fd029ea09a7de128144285c9
Eric Hedgren <ehedgren@iastate.edu>
2023-10-13 16:15:33 -0500

    still working on mmbf

--- Main2.java
+++ Main2.java
@@ -1,7 +1,7 @@
 public class Main2 {
 
     public static void main(String args[]){
-        BloomFilterRan bf = new BloomFilterRan(100, 10);
+        MultiMultiBloomFilter bf = new MultiMultiBloomFilter(100, 10);
         for (int i = 0; i < 100; i++){
             String word = "hi" + i;
             bf.add(word);
--- MultiMultiBloomFilter.java
+++ MultiMultiBloomFilter.java
@@ -1,2 +1,100 @@
+import java.util.BitSet;
+import java.util.Random;
+
 public class MultiMultiBloomFilter {
+    public int setSize;
+
+    public int bitsPerElement;
+
+    BitSet[] multiBitArr;
+    public BitSet bitArray;
+
+    public int dataSize;
+
+    public MultiMultiBloomFilter(int setSize, int bitsPerElement) {
+        this.setSize = setSize;
+        this.bitsPerElement = bitsPerElement;
+        multiBitArr = new BitSet[filterSize()];
+        for (int i = 0; i < filterSize(); i++) {
+            multiBitArr[i] = new BitSet();
+        }
+        dataSize = 0;
+    }
+
+    public void add (String s){
+        s = s.toLowerCase();
+        double x = numHashes();
+        for(int i = 0; i < numHashes(); i++){
+            int index = mmHash(s, i);
+            multiBitArr[i].set(index);
+        }
+        dataSize++;
+    }
+
+    public boolean appears (String s){
+        s = s.toLowerCase();
+        double x = numHashes();
+        boolean flag = true;
+        for(int i = 0; i < numHashes(); i++){
+            int index = mmHash(s, i);
+            if (this.getBit(index) == false){
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+    public int filterSize() {
+        return setSize*bitsPerElement;
+    }
+
+    public int dataSize() {
+        return dataSize;
+    }
+
+    public double numHashes() {
+        return (Math.log(2) * filterSize()) / setSize;
+    }
+
+    public boolean getBit(int j) {
+        for (int i = 0; i < filterSize(); i++) {
+            if (!multiBitArr[i].get(j)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int mmHash (String s, int i) {
+        int m = this.filterSize();
+        int p = findPrimeLargerThanM(m, i);
+        int x = s.hashCode();
+        x = Math.abs(x);
+
+        // the first iteration i=0, but i must be positive
+        Random rand = new Random(i);
+        int a = rand.nextInt(i+1);
+        int b = rand.nextInt(i+1);
+
+        return (a * x + b)%p;
+    }
+
+    public int findPrimeLargerThanM(int m, int i){
+        Random rand = new Random(i);
+        int prime = rand.nextInt(100) + m;
+        while(!isPrime(prime)){
+            prime = rand.nextInt(100) + m;
+        }
+        return prime;
+    }
+
+    public boolean isPrime(int n){
+        boolean flag = true;
+        for (int i = 2; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                flag = false;
+            }
+        }
+        return flag;
+    }
 }
